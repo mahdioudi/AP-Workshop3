214e3bba8a06d796ca69da662b3d3dc2aaa44a3a
mahdioudi <mahdioudi5@gmail.com>
2024-03-17 20:21:22 +0330

    add getter and setter to address class

--- src/Address.java
+++ src/Address.java
@@ -12,17 +12,34 @@ public class Address {
                 '}';
     }
 
-    public String Address(String zipcode , String country , String city){
-        this.city = city;
-        this.country = country;
+
+    public Address(String zipcode, String country, String city) {
         this.zipcode = zipcode;
+        this.country = country;
+        this.city = city;
+    }
 
+    public String getZipcode() {
+        return zipcode;
+    }
 
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
 
+    public String getCountry() {
+        return country;
+    }
 
+    public void setCountry(String country) {
+        this.country = country;
+    }
 
+    public String getCity() {
+        return city;
+    }
 
-
-
+    public void setCity(String city) {
+        this.city = city;
     }
 }

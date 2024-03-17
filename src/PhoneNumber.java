96856fe69182d2e77c2daae2e2bbfcb8c2ec5376
mahdioudi <mahdioudi5@gmail.com>
2024-03-17 20:26:35 +0330

    add getter and setter to phonenumber class

--- src/PhoneNumber.java
+++ src/PhoneNumber.java
@@ -14,4 +14,20 @@ public class PhoneNumber {
                 ", number='" + number + '\'' +
                 '}';
     }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 }

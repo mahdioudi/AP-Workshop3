64c55397bb214611de2d179aae40d54da9d40536
mahdioudi <mahdioudi5@gmail.com>
2024-03-17 20:24:11 +0330

    add fields of phonenumber class and add constructor for that class

--- src/PhoneNumber.java
+++ src/PhoneNumber.java
@@ -1,3 +1,9 @@
 public class PhoneNumber {
-    private
+    private String countryCode;
+    private String number;
+
+    public PhoneNumber(String countryCode, String number) {
+        this.countryCode = countryCode;
+        this.number = number;
+    }
 }

e4c8711d0a532037b51e5002d0e8260c6646a6fc
mahdioudi <mahdioudi5@gmail.com>
2024-03-17 20:16:22 +0330

    add constructor for address class

--- src/Address.java
+++ src/Address.java
@@ -2,4 +2,11 @@ public class Address {
     private String zipcode;
     private String country;
     private String city;
+
+    public Address(String zipcode , String country , String city){
+        this.city = city;
+        this.country = country;
+        this.zipcode = zipcode;
+
+    }
 }
